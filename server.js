aa286a463243815c96fd5d7907fad5337a5a811a
Sardaar2003 <singhmantej536@gmail.com>
2024-06-01 10:27:40 +0530

    Changes in the server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 // Mongo DB Connection 
 require("./config/db");
 
+
+//CORS POLICY
+const cors=require("cors");
 const app=require("express")();
 const bodyParser=require("express").json;
 const port=8080;
 const UserRouter=require("./api/User");
 
 app.use(bodyParser());
-
+app.use(cors());
 app.use("/user",UserRouter);
 
 app.listen(port,()=>{
