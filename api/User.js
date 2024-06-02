34e6568f8327ebf86e3d0c7952d663b5b010a091
Sardaar2003 <singhmantej536@gmail.com>
2024-06-02 12:29:25 +0530

    Changes in Ports

--- api/User.js
+++ api/User.js
@@ -148,7 +148,7 @@ const sendVerificationEmail= async ({_id,email},res)=>
 {
         
         //url to be used in the email
-        const currentUrl="http://localhost:8080/";
+        const currentUrl="http://localhost:5000/";
         const uniqueString=uuidv4()+_id;
         const mailOptions=
         {
